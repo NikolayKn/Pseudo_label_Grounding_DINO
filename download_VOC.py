e2878bcebcf5343571c38ebfe53e3024961a840c
Nikolay <nikolay1s112@gmail.com>
2024-03-30 18:46:13 +0300

    fixed saving dirs

--- download_VOC.py
+++ download_VOC.py
@@ -5,7 +5,59 @@ from pathlib import Path
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
 
+
+
+
+
+def convert_label(path, lb_path, year, image_id, class_names):
+    def convert_box(size, box):
+        dw, dh = 1. / size[0], 1. / size[1]
+        x, y, w, h = (box[0] + box[1]) / 2.0 - 1, (box[2] + box[3]) / 2.0 - 1, box[1] - box[0], box[3] - box[2]
+        return x * dw, y * dh, w * dw, h * dh
+
+    in_file = open(path / f'VOC{year}/Annotations/{image_id}.xml')
+    out_file = open(lb_path, 'w')
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        cls = obj.find('name').text
+        if cls in class_names and int(obj.find('difficult').text) != 1:
+            xmlbox = obj.find('bndbox')
+            bb = convert_box((w, h), [float(xmlbox.find(x).text) for x in ('xmin', 'xmax', 'ymin', 'ymax')])
+            cls_id = class_names.index(cls)  # class id
+            out_file.write(" ".join(str(a) for a in (cls_id, *bb)) + '\n')
+
+
 def main():
+
+
+    class_names= [
+        'aeroplane',
+        'bicycle',
+        'bird',
+        'boat',
+        'bottle',
+        'bus',
+        'car',
+        'cat',
+        'chair',
+        'cow',
+        'diningtable',
+        'dog',
+        'horse',
+        'motorbike',
+        'person',
+        'pottedplant',
+        'sheep',
+        'sofa',
+        'train',
+        'tvmonitor'
+    ]
+    
     # Download
     if os.path.isdir('datasets/VOC/VOCdevkit'):
         print('VOC dataset already exists. \nTo download the VOC dataset again remove  "datasets/VOC/VOCdevkit" directory ')
@@ -43,6 +95,8 @@ def main():
         for id in tqdm(image_ids, desc=f'{image_set}{year}'): 
             f = path + f'/VOC{year}/JPEGImages/{id}.jpg'  # old img path 
             f = Path(f)
+            lb_path = (lbs_path / f.name).with_suffix('.txt')  # new label path 
+            convert_label(Path(path), Path(lb_path), year, id, class_names)  # convert labels to YOLO format
             f.rename(imgs_path / f.name)  # move image
 
 if __name__ == "__main__":
